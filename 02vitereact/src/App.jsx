a11ce1394c0e64266094c9decd1ba9a0be846341
shatakshi <shatakshi.1702@gmail.com>
2026-06-13 15:30:56 +0530

    initial React workspace

---
+++ 02vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+   <>
+   <h1>react with chai aur code</h1>
+   <Chai />
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, doloribus?</p>
+   </>
+  )
+}
+
+export default App
